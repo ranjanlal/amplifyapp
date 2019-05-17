37df7b69ae4d77873f896dd7448fac9acbb1ae3b
Ranjan Lal <ranjanlal@Ranjans-MacBook-Air.local>
2019-05-17 11:57:07 +1000

    Added tic-tac-toe game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game from './tic_tac_toe';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          This is Develop branch !!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Tic Tac Toe</h2>
+        <Game />
       </header>
     </div>
   );
